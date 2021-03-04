1b16f594cb25eb426e64075c78cae8957a663a1c
Joel Irineu <irineu.joel01@gmail.com>
2021-03-04 19:34:09 -0300

    adicionando cors para que qualquer aplicação acesse o backend

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const path = require('path')
+const cors = require('cors')
 
 const app = express()
 
@@ -8,6 +9,8 @@ mongoose.connect('mongodb+srv://root:jo2603@omnistack.q9hib.mongodb.net/semana07
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
+
+app.use(cors())
 
 app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads', 'resized')))
 
