bdaa43eed0094d74116c8f1d7e223d222f603e88
Joel Irineu <irineu.joel01@gmail.com>
2021-03-03 23:31:14 -0300

    configuração do banco de dados mongoDB e do mongoose

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 const express = require('express')
+const mongoose = require('mongoose')
 
 const app = express()
+
+mongoose.connect('mongodb+srv://root:jo2603@omnistack.q9hib.mongodb.net/semana07?retryWrites=true&w=majority',{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
 
 app.get("/", (req, res) =>{
     return res.send(`irineu, vc não sabe e nem eu!`)
