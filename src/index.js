06f34c1cbe171e5c16cbdfb98f583a2c25d0ddc6
Joel Irineu <irineu.joel01@gmail.com>
2021-03-04 19:28:41 -0300

    configuração para que o server retorne o caminho da imagem e mude a extensão para jpg

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const path = require('path')
 
 const app = express()
 
@@ -7,6 +8,9 @@ mongoose.connect('mongodb+srv://root:jo2603@omnistack.q9hib.mongodb.net/semana07
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
+
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads', 'resized')))
+
 app.use(require('./routes'))
 
 
